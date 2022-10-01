4b91361f9bb63a1cee828e15e77e3de4f245d4c7
nameruse <nameruse@protonmail.com>
2022-10-01 09:52:16 +0100

    added zsh config

--- install.sh
+++ install.sh
@@ -6,3 +6,10 @@
 mkdir -p "$HOME/.config/nvim"
 mkdir -p "$HOME/.config/nvim/undo"
 ln -sf "$HOME/dotfiles/nvim/init.vim" "$HOME/.config/nvim"
+
+#######
+# zsh #
+#######
+mkdir -p "$HOME/.config/zsh"
+ln -sf "$HOME/dotfiles/zsh/.zshenv" "$HOME"
+ln -sf "$HOME/dotfiles/zsh/.zshrc" "$HOME/.config/zsh"
